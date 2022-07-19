780eb89d04e9e082a74d9459a494827c7e5567bf
vyom <sharm843@umn.edu>
2022-07-19 13:45:13 +0530

    Created hard-coded popuo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import ReactMapGL, { Marker } from 'react-map-gl';
-import { Room } from '@material-ui/icons';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
+import { Room, Star } from '@material-ui/icons';
+import "../src/app.css"
 
 function App() {
     const [viewport, setViewport] = useState({
@@ -18,9 +19,36 @@ function App() {
             mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
             onViewportChange={nextViewport => setViewport(nextViewport)}
             mapStyle="mapbox://styles/vyom1611/cl5qlsd9k006115nxmppra8sr">
-            <Marker latitude={37.78} longitude={-122.41} offsetLeft={-20} offsetTop={-10}>
+            <Marker latitude={37.78} 
+                    longitude={-122.41} 
+                    offsetLeft={-20} 
+                    offsetTop={-10}>
                 <Room style={{fontSize:viewport.zoom*6, color:"slateblue"}}/>
             </Marker>
+            <Popup
+                latitude={37.78}
+                longitude={-122.41}
+                closeButton={true}
+                closeOnClick={false}
+                anchor="top">
+            <div className='card'>
+                <label>Place</label>
+                <h4 className='place'>Downtown San Francisco</h4>
+                <label>Review</label>
+                <p className='desc'>Natural beauty and great city!</p>
+                <label>Rating</label>
+                <div className="stars">
+                    <Star className='star'/>
+                    <Star className='star'/>
+                    <Star className='star'/>
+                    <Star className='star'/>
+                    <Star className='star'/>
+                </div>
+                <label>Information</label>
+                <span className="username"> Created by <b>Vyom</b> </span>
+                <span className="date"> 1 hour ago</span>
+            </div>
+            </Popup>
             </ReactMapGL>
         </div>
     )
